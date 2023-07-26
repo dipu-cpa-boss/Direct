82fa90ff04a47fc7bf2d3bbe08f94622361abcb5
dipu islam <121944271+dipu-cpa-boss@users.noreply.github.com>
2023-07-26 22:47:07 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "https://wabxeq.flndmyluve.net/?utm_source=da57dc555e50572d&s1=193358&s2=1851132&s3=Baby&s5=Baby&j1=1&j5=1"
+const offerLink = "https://kedmgb.datlngllfe.net/?utm_source=da57dc555e50572d&s1=197700&s2=1902241&s3=baby&s5=sex&j1=1"
