41e2007ac6746dfafaaa9225f7a53b602c777314
dipu islam <121944271+dipu-cpa-boss@users.noreply.github.com>
2023-07-28 01:09:58 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,1 +1,1 @@
-const offerLink = "https://kedmgb.datlngllfe.net/?utm_source=da57dc555e50572d&s1=197700&s2=1902241&s3=baby&s5=sex&j1=1"
+const offerLink = "https://sweetiemeet.com/tds/ae?tdsId=s0792tok_r&tds_campaign=s0792tok&utm_sub=opnfnl&s1=ps&utm_source=int&affid=03585827&subid=baby&clickid=sex"
